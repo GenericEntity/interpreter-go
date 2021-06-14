65bd512a8aa90b70d1f94cde5cf7cc5d93936854
GenericEntity <genericentity01@gmail.com>
2021-06-15 01:18:50 +0800

    REPL: Update to read-parse-print-loop

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/GenericEntity/interpreter-go/monkey/lexer"
-	"github.com/GenericEntity/interpreter-go/monkey/token"
+	"github.com/GenericEntity/interpreter-go/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -25,9 +25,23 @@ func Start(in io.Reader, out io.Writer) {
 		// Eval
 		line := scanner.Text()
 		lex := lexer.New(line)
+		p := parser.New(lex)
 
-		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Yikes! Watch what you type!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
