b0d35b3b34ac03aa560468f8615e4d048a9cabdd
GenericEntity <genericentity01@gmail.com>
2021-06-15 23:46:37 +0800

    REPL: Add evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/GenericEntity/interpreter-go/monkey/evaluator"
 	"github.com/GenericEntity/interpreter-go/monkey/lexer"
 	"github.com/GenericEntity/interpreter-go/monkey/parser"
 )
@@ -33,8 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
