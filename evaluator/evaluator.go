6849e5affa7e29f2681c4d0d1cc56ae32a44886a
GenericEntity <genericentity01@gmail.com>
2021-06-15 23:38:24 +0800

    Eval: Add failing test case for Eval on integers

---
+++ evaluator/evaluator.go
@@ -0,0 +1,10 @@
+package evaluator
+
+import (
+	"github.com/GenericEntity/interpreter-go/monkey/ast"
+	"github.com/GenericEntity/interpreter-go/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	return nil
+}
---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,47 @@
+package evaluator
+
+import (
+	"testing"
+
+	"github.com/GenericEntity/interpreter-go/monkey/lexer"
+	"github.com/GenericEntity/interpreter-go/monkey/object"
+	"github.com/GenericEntity/interpreter-go/monkey/parser"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	lex := lexer.New(input)
+	p := parser.New(lex)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. expected=%d, got=%d", expected, result.Value)
+		return false
+	}
+
+	return true
+}
