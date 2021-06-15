f965c18de33d3bdf2ebbdd4ae4269481148e64a4
GenericEntity <genericentity01@gmail.com>
2021-06-15 23:50:28 +0800

    Eval: Add failing tests for boolean literals

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -45,3 +45,33 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	return true
 }
+
+func TestBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. expected=%t, got=%t", expected, result.Value)
+		return false
+	}
+
+	return true
+}
