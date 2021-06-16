9aa6c35eea0f710dbb3460006d746dc1f44c61a6
GenericEntity <genericentity01@gmail.com>
2021-06-17 00:22:24 +0800

    Eval: Add support for integer infix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -30,6 +30,11 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 
 	return nil
@@ -83,4 +88,31 @@ func evalMinusPrefixOperatorExpression(operand object.Object) object.Object {
 
 	value := operand.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	lValue := left.(*object.Integer).Value
+	rValue := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: lValue + rValue}
+	case "-":
+		return &object.Integer{Value: lValue - rValue}
+	case "*":
+		return &object.Integer{Value: lValue * rValue}
+	case "/":
+		return &object.Integer{Value: lValue / rValue}
+	default:
+		return NULL
+	}
 }
