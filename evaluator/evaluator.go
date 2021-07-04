7c9cee94e3678f2f6bbe73de216fa4936e9f3510
GenericEntity <genericentity01@gmail.com>
2021-07-04 12:09:01 +0800

    Eval: Add support for function literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -69,6 +69,15 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{
+			Parameters: params,
+			Body:       body,
+			Env:        env,
+		}
 	}
 
 	return nil
