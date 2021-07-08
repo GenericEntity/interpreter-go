794dd461dbfebcd7cef6a8edde7c1a50c1ee8025
GenericEntity <genericentity01@gmail.com>
2021-07-09 00:50:33 +0800

    Eval: Add support for string literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -91,6 +91,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return applyFunction(fn, args)
+
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+
 	}
 
 	return nil
