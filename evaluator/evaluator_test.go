19cbdbe6f19539d2b3121864f87614666d514f0f
GenericEntity <genericentity01@gmail.com>
2021-07-09 00:45:04 +0800

    Eval: Fix failing test case (due to formatting)

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -317,7 +317,7 @@ func TestFunctionObject(t *testing.T) {
 		t.Fatalf("parameter is not 'x'. got=%q", fn.Parameters[0])
 	}
 
-	expectedBody := "{\n\t(x + 2);\n}"
+	expectedBody := "(x + 2)"
 
 	if fn.Body.String() != expectedBody {
 		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
