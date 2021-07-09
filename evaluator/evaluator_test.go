47916ac77af8a7d28608d4fd32246423da43aa6b
GenericEntity <genericentity01@gmail.com>
2021-07-09 20:14:55 +0800

    Eval: Add failing tests for string concatenation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -267,6 +267,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar;",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -354,5 +358,19 @@ func TestStringLiteral(t *testing.T) {
 
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. expected=%q. got=%q", "Hello World!", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
