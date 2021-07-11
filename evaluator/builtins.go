38f98b3f2e4b43e381cff1bf3269b2cf29729958
GenericEntity <genericentity01@gmail.com>
2021-07-11 17:42:40 +0800

    Eval: Extract error generation in len()

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -2,11 +2,22 @@ package evaluator
 
 import "github.com/GenericEntity/interpreter-go/monkey/object"
 
+func checkArgsLen(wantedLength int, args ...object.Object) *object.Error {
+	if len(args) != wantedLength {
+		return newError("wrong number of arguments. got=%d, want=%d", len(args), wantedLength)
+	}
+	return nil
+}
+
+func newTypeNotSupportedError(funcName string, arg object.Object) *object.Error {
+	return newError("argument to `%s` not supported, got %s", funcName, arg.Type())
+}
+
 var builtins = map[string]*object.Builtin{
 	"len": {
 		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			if err := checkArgsLen(1, args...); err != nil {
+				return err
 			}
 
 			switch arg := args[0].(type) {
@@ -17,7 +28,7 @@ var builtins = map[string]*object.Builtin{
 				return &object.Integer{Value: int64(len(arg.Elements))}
 
 			default:
-				return newError("argument to `len` not supported, got %s", arg.Type())
+				return newTypeNotSupportedError("len", arg)
 			}
 		},
 	},
